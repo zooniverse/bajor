c4db95517d2da95e06c0122764ee149d0a9b3f7a
Campbell Allen <campbell.allen@gmail.com>
2022-06-01 21:46:27 +0100

    add dev app reloading

--- bajor/main.py
+++ bajor/main.py
@@ -8,5 +8,16 @@ def root():
     return { "revision": os.environ.get('REVISION') }
 
 def start_app():
-    uvicorn.run("bajor.main:app", host="0.0.0.0",
-                port=int(os.environ.get('PORT', '8000')))
+    uvicorn.run(
+        "bajor.main:app",
+        host=os.environ.get('HOST', '0.0.0.0'),
+        port=int(os.environ.get('PORT', '8000'))
+    )
+
+def start_dev_app():
+    uvicorn.run(
+        "bajor.main:app",
+        host=os.environ.get('HOST', '0.0.0.0'),
+        port=int(os.environ.get('PORT', '8000')),
+        reload=True
+    )
