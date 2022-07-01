b1e19e32d537dc926dba3b33f51b787d9c1255c4
Campbell Allen <campbell.allen@gmail.com>
2022-07-01 11:06:59 +0100

    add API skeleton

--- bajor/main.py
+++ bajor/main.py
@@ -1,14 +1,50 @@
-from tracemalloc import start
-import uvicorn, os
-from fastapi import FastAPI
+import uvicorn, os, secrets
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from pydantic import BaseModel
 from honeybadger import contrib
+
+if os.getenv('DEBUG'):
+  import pdb
+
+# add the basic auth setup
+# https://fastapi.tiangolo.com/advanced/security/http-basic-auth/#simple-http-basic-auth
 
 app = FastAPI()
 # configure HB through the env vars https://docs.honeybadger.io/lib/python/#configuration
 app.add_middleware(contrib.ASGIHoneybadger)
+# add the http basic authorization mode
+security = HTTPBasic()
+
+class Job(BaseModel):
+    manifest_path: str
+    job_id: str | None
+    scheduled: str | None
+
+def validate_basic_auth(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(
+        credentials.username, os.environ.get('BASIC_AUTH_USERNAME', 'bajor'))
+    correct_password = secrets.compare_digest(
+        credentials.password, os.environ.get('BASIC_AUTH_PASSWORD', 'bajor'))
+    if not (correct_username and correct_password):
+        return False
+    else:
+      return True
+
+@app.post("/jobs/", status_code=status.HTTP_201_CREATED)
+async def schedule_job(job: Job, authorized: bool = Depends(validate_basic_auth)):
+  if not authorized:
+    raise HTTPException(
+      status_code=status.HTTP_401_UNAUTHORIZED,
+      detail="Incorrect username or password",
+      headers={"WWW-Authenticate": "Basic"},
+    )
+  # TODO: this is where we schedule ze job!
+  # embedd the scheduling information into the Job model
+  return job
 
 @app.get("/")
-def root():
+async def root():
     return { "revision": os.environ.get('REVISION') }
 
 def start_app(reload=False):
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,6 +3,8 @@ import uuid, os
 from unittest import mock
 from bajor.main import app
 
+import pdb
+
 client = TestClient(app)
 fake_revision = str(uuid.uuid4())
 
@@ -12,3 +14,65 @@ def test_read_main():
     assert response.status_code == 200
     assert response.json() == {"revision": fake_revision}
 
+
+# how to setup authorization code for fast API (JWT)
+# https://auth0.com/blog/build-and-secure-fastapi-server-with-auth0/
+# how to skip auth for testing
+# https: // stackoverflow.com/questions/71457448/how-can-i-skip-authentication-in-a-single-test-with-a-fixture-in-fast-api-togeth
+# how to setup a create route
+# https://fastapi.tiangolo.com/tutorial/testing/#extended-testing-file
+# post the manifest path
+# pass some basic auth creds
+# test the response object
+# and test the bajor interface is called
+def test_manifest_job_creation_without_auth_creds():
+    response = client.post(
+        "/jobs/",
+        json={"manifest_path": "test_manifest_file_path.csv"},
+    )
+    assert response.status_code == 401
+    assert response.json() == { "detail": "Not authenticated"}
+
+
+@mock.patch.dict(os.environ, {"BASIC_AUTH_USERNAME": 'bajor', "BASIC_AUTH_PASSWORD": 'bajor'})
+def test_manifest_job_creation_incorrect_auth_creds():
+    response = client.post(
+        "/jobs/",
+        auth=('test', 'test'),
+        json={"manifest_path": "test_manifest_file_path.csv"},
+    )
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Incorrect username or password"}
+
+def test_manifest_job_creation_correct_auth_creds():
+    response = client.post(
+        "/jobs/",
+        auth=('bajor', 'bajor'),
+        json={"manifest_path": "test_manifest_file_path.csv"},
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        'manifest_path': 'test_manifest_file_path.csv', 'job_id': None, 'scheduled': None}
+
+@mock.patch.dict(os.environ, {"BASIC_AUTH_USERNAME": 'rojab', "BASIC_AUTH_PASSWORD": 'rojab'})
+def test_authentication_env_var_configs():
+    response = client.post(
+        "/jobs/",
+        auth=('rojab', 'rojab'),
+        json={"manifest_path": "test_manifest_file_path.csv"},
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        'manifest_path': 'test_manifest_file_path.csv', 'job_id': None, 'scheduled': None}
+
+
+@mock.patch.dict(os.environ, {"BASIC_AUTH_USERNAME": 'rojab', "BASIC_AUTH_PASSWORD": 'rojab'})
+def test_batch_code_is_called():
+    response = client.post(
+        "/jobs/",
+        auth=('rojab', 'rojab'),
+        json={"manifest_path": "test_manifest_file_path.csv"},
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        'manifest_path': 'test_manifest_file_path.csv', 'job_id': None, 'scheduled': None}
