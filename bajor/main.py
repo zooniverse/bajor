8e0bcf0a5c04517df0ccfe9588b3f1ea4d1a411a
Campbell Allen <campbell.allen@gmail.com>
2022-07-01 16:48:45 +0100

    add detailed Job model resposne data

--- bajor/main.py
+++ bajor/main.py
@@ -1,4 +1,7 @@
-import uvicorn, os, secrets
+import uvicorn
+import os
+import secrets
+import uuid
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from pydantic import BaseModel
@@ -7,24 +10,23 @@ from logging.config import dictConfig
 from bajor.batch import schedule_job
 from bajor.log_config import log_config
 
+
 if os.getenv('DEBUG'):
   import pdb
 
 dictConfig(log_config)
 
-# add the basic auth setup
-# https://fastapi.tiangolo.com/advanced/security/http-basic-auth/#simple-http-basic-auth
-
 app = FastAPI()
 # configure HB through the env vars https://docs.honeybadger.io/lib/python/#configuration
 app.add_middleware(contrib.ASGIHoneybadger)
 # add the http basic authorization mode
+# https://fastapi.tiangolo.com/advanced/security/http-basic-auth/#simple-http-basic-auth
 security = HTTPBasic()
 
 class Job(BaseModel):
     manifest_path: str
-    job_id: str | None
-    scheduled: str | None
+    id: str | None
+    status: str | None
 
 def validate_basic_auth(credentials: HTTPBasicCredentials = Depends(security)):
     correct_username = secrets.compare_digest(
@@ -46,6 +48,11 @@ async def create_job(job: Job, authorized: bool = Depends(validate_basic_auth)):
     )
   # TODO: this is where we schedule ze job!
   # embedd the scheduling information into the Job model
+  job_id = str(uuid.uuid4())
+
+  results = schedule_job(job_id, job.manifest_path)
+  job.id = results['submitted_job_id']
+  job.status = results['job_task_status']
 
   return job
 
