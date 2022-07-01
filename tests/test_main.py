9ca927502ad318bfed7877a2e856f5d71982a286
Campbell Allen <campbell.allen@gmail.com>
2022-07-01 16:48:14 +0100

    setup mocked test client app for schedule_job function

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,32 +1,36 @@
 from fastapi.testclient import TestClient
-import uuid, os
+import uuid, os, pytest
 from unittest import mock
-from bajor.main import app
 
-import pdb
-
-client = TestClient(app)
 fake_revision = str(uuid.uuid4())
+submitted_job_id = 'fake-job-id'
+
+
+@pytest.fixture
+def mocked_client(autorun=True):
+  """ Setup a TestClient API application with the mocked batch.py job scheduling function """
+  with mock.patch('bajor.batch.schedule_job') as mock_schedule_job:
+
+    result_set = {"submitted_job_id": submitted_job_id,
+                  "job_task_status": {"status": "started", "message": "Job submitted successfully"}}
+
+    mock_schedule_job.return_value = result_set
+
+    # ensure we import this code after patching the batch#schedule_job function
+    from bajor.main import app
+    client = TestClient(app)
+
+    yield client
+
 
 @mock.patch.dict(os.environ, {"REVISION": fake_revision})
-def test_read_main():
-    response = client.get("/")
+def test_read_main(mocked_client):
+    response = mocked_client.get("/")
     assert response.status_code == 200
     assert response.json() == {"revision": fake_revision}
 
-
-# how to setup authorization code for fast API (JWT)
-# https://auth0.com/blog/build-and-secure-fastapi-server-with-auth0/
-# how to skip auth for testing
-# https: // stackoverflow.com/questions/71457448/how-can-i-skip-authentication-in-a-single-test-with-a-fixture-in-fast-api-togeth
-# how to setup a create route
-# https://fastapi.tiangolo.com/tutorial/testing/#extended-testing-file
-# post the manifest path
-# pass some basic auth creds
-# test the response object
-# and test the bajor interface is called
-def test_manifest_job_creation_without_auth_creds():
-    response = client.post(
+def test_manifest_job_creation_without_auth_creds(mocked_client):
+    response = mocked_client.post(
         "/jobs/",
         json={"manifest_path": "test_manifest_file_path.csv"},
     )
@@ -35,8 +39,8 @@ def test_manifest_job_creation_without_auth_creds():
 
 
 @mock.patch.dict(os.environ, {"BASIC_AUTH_USERNAME": 'bajor', "BASIC_AUTH_PASSWORD": 'bajor'})
-def test_manifest_job_creation_incorrect_auth_creds():
-    response = client.post(
+def test_manifest_job_creation_incorrect_auth_creds(mocked_client):
+    response = mocked_client.post(
         "/jobs/",
         auth=('test', 'test'),
         json={"manifest_path": "test_manifest_file_path.csv"},
@@ -44,35 +48,34 @@ def test_manifest_job_creation_incorrect_auth_creds():
     assert response.status_code == 401
     assert response.json() == {"detail": "Incorrect username or password"}
 
-def test_manifest_job_creation_correct_auth_creds():
-    response = client.post(
+def test_manifest_job_creation_correct_auth_creds(mocked_client):
+    response = mocked_client.post(
         "/jobs/",
         auth=('bajor', 'bajor'),
         json={"manifest_path": "test_manifest_file_path.csv"},
     )
     assert response.status_code == 201
     assert response.json() == {
-        'manifest_path': 'test_manifest_file_path.csv', 'job_id': None, 'scheduled': None}
+        'manifest_path': 'test_manifest_file_path.csv', 'id': submitted_job_id, 'status': {"status": "started", "message": "Job submitted successfully"}}
 
 @mock.patch.dict(os.environ, {"BASIC_AUTH_USERNAME": 'rojab', "BASIC_AUTH_PASSWORD": 'rojab'})
-def test_authentication_env_var_configs():
-    response = client.post(
+def test_authentication_env_var_configs(mocked_client):
+    response = mocked_client.post(
         "/jobs/",
         auth=('rojab', 'rojab'),
         json={"manifest_path": "test_manifest_file_path.csv"},
     )
     assert response.status_code == 201
     assert response.json() == {
-        'manifest_path': 'test_manifest_file_path.csv', 'job_id': None, 'scheduled': None}
+        'manifest_path': 'test_manifest_file_path.csv', 'id': submitted_job_id, 'status': {"status": "started", "message": "Job submitted successfully"}}
 
 
-@mock.patch.dict(os.environ, {"BASIC_AUTH_USERNAME": 'rojab', "BASIC_AUTH_PASSWORD": 'rojab'})
-def test_batch_code_is_called():
-    response = client.post(
+def test_batch_scheduling_code_is_called(mocked_client):
+    response = mocked_client.post(
         "/jobs/",
-        auth=('rojab', 'rojab'),
+        auth=('bajor', 'bajor'),
         json={"manifest_path": "test_manifest_file_path.csv"},
     )
     assert response.status_code == 201
     assert response.json() == {
-        'manifest_path': 'test_manifest_file_path.csv', 'job_id': None, 'scheduled': None}
+        'manifest_path': 'test_manifest_file_path.csv', 'id': submitted_job_id, 'status': {"status": "started", "message": "Job submitted successfully"}}
