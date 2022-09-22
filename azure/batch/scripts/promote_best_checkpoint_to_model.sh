d7711ddf61e0733b4a241a409524c15c6c24ed77
Campbell Allen <campbell.allen@gmail.com>
2022-09-22 16:57:46 +0300

    add azure batch scripts
    these are stored on the azure blob storage training container under the code/ path
    
    these are injected into the batch container at runtime to run the training and promote the trained model

---
+++ azure/batch/scripts/promote_best_checkpoint_to_model.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo 
+echo 
+echo '####### SAVE AND PROMOTE BEST CHECKPOINT TO MODEL #######'
+echo 
+
+set -ex
+
+# find the lowest loss checkpoint file path (without suffix)
+best_checkpoint=$(find $1 -name *.ckpt -exec basename {} .ckpt \; | sort -t . -k 2 -n | head -n 1 | xargs -I {} find $1 -name {}.ckpt)
+echo "the checkpoint with the lowest loss value is: $best_checkpoint"
+
+# create a filename that links the job to the checkpoint resutls
+mkdir -p $AZ_BATCH_NODE_MOUNTS_DIR/$CONTAINER_MOUNT_DIR/models/$TRAINING_JOB_RESULTS_DIR
+
+# move the checkpoint to the models job dir for long terms storage (tracking)
+cp -f $best_checkpoint $AZ_BATCH_NODE_MOUNTS_DIR/$CONTAINER_MOUNT_DIR/models/$TRAINING_JOB_RESULTS_DIR/
+
+# overwrite the existing deployed model for use in other services (prediction scripts etc)
+cp -f $best_checkpoint $AZ_BATCH_NODE_MOUNTS_DIR/$CONTAINER_MOUNT_DIR/models/zoobot.ckpt
