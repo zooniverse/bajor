6443ff91ba06be1bcdb69faf1429e52a3d5034a6
Campbell Allen <campbell.allen@gmail.com>
2022-09-23 18:44:13 +0300

    update promote checkpoint to use models container

--- azure/batch/scripts/promote_best_checkpoint_to_model.sh
+++ azure/batch/scripts/promote_best_checkpoint_to_model.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo 
-echo 
+echo
+echo
 echo '####### SAVE AND PROMOTE BEST CHECKPOINT TO MODEL #######'
-echo 
+echo
 
 set -ex
 
@@ -12,10 +12,10 @@ best_checkpoint=$(find $1 -name *.ckpt -exec basename {} .ckpt \; | sort -t . -k
 echo "the checkpoint with the lowest loss value is: $best_checkpoint"
 
 # create a filename that links the job to the checkpoint resutls
-mkdir -p $AZ_BATCH_NODE_MOUNTS_DIR/$CONTAINER_MOUNT_DIR/models/$TRAINING_JOB_RESULTS_DIR
+mkdir -p $AZ_BATCH_NODE_MOUNTS_DIR/$MODELS_CONTAINER_MOUNT_DIR/$TRAINING_JOB_RESULTS_DIR
 
 # move the checkpoint to the models job dir for long terms storage (tracking)
-cp -f $best_checkpoint $AZ_BATCH_NODE_MOUNTS_DIR/$CONTAINER_MOUNT_DIR/models/$TRAINING_JOB_RESULTS_DIR/
+cp -f $best_checkpoint $AZ_BATCH_NODE_MOUNTS_DIR/$MODELS_CONTAINER_MOUNT_DIR/$TRAINING_JOB_RESULTS_DIR/
 
 # overwrite the existing deployed model for use in other services (prediction scripts etc)
-cp -f $best_checkpoint $AZ_BATCH_NODE_MOUNTS_DIR/$CONTAINER_MOUNT_DIR/models/zoobot.ckpt
+cp -f $best_checkpoint $AZ_BATCH_NODE_MOUNTS_DIR/$MODELS_CONTAINER_MOUNT_DIR/zoobot.ckpt
