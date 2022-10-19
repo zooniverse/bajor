7af1b2a037d4d4368e81d30609d2011b83adc48d
Campbell Allen <campbell.allen@gmail.com>
2022-10-19 14:43:43 +0700

    add Prediction and Training jobs

--- bajor/models/job.py
+++ bajor/models/job.py
@@ -1,7 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
 
-class Job(BaseModel):
+class TrainingJob(BaseModel):
     manifest_path: str
+    id: str | None
+    status: str | None
+    run_opts: str = ''
+
+
+class PredictionJob(BaseModel):
+    manifest_url: HttpUrl
     id: str | None
     status: str | None
     run_opts: str = ''
