4c0f53abf3adfb98d77662f1b796b6f72af88b01
Campbell Allen <campbell.allen@gmail.com>
2022-11-23 13:16:16 +0700

    use finetune api dev version of zoobot
    once this is merged to main we can switch back to the published pypi package

--- azure/batch/setup.py
+++ azure/batch/setup.py
@@ -19,7 +19,8 @@ setuptools.setup(
     python_requires=">=3.7",  # tf 2.8.0 requires Python 3.7 and above
     install_requires=[
         # 'zoobot[pytorch] >= 0.0.4', # the big cheese - bring in the zoobot!
-        'zoobot[pytorch] @ git+https://github.com/mwalmsley/zoobot.git@dev',
+        # 'zoobot[pytorch] @ git+https://github.com/mwalmsley/zoobot.git@dev',
+        'zoobot[pytorch] @ git+https://github.com/camallen/zoobot.git@param-finetuning',
         'requests >= 2.28.1', # used to download prediction images from a remote URL
         'h5py >= 3.7.0' # used for prediction exports
     ]
