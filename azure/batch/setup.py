23a3b397e16b5b0028577a21a695fd27c87fb0f1
Campbell Allen <campbell.allen@gmail.com>
2023-02-15 15:07:02 +1000

    use Mike's for-v1 dev branch
    this is the latest branch for v1 release so we want to use it while we prepare for v1 release

--- azure/batch/setup.py
+++ azure/batch/setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     python_requires=">=3.7",  # tf 2.8.0 requires Python 3.7 and above
     install_requires=[
         # 'zoobot[pytorch] >= 0.0.4', # the big cheese - bring in the zoobot!
-        'zoobot[pytorch_cu113] @ git+https://github.com/mwalmsley/zoobot.git@dev', # use mike's dev branch till v1.0 is released
+        'zoobot[pytorch_cu113] @ git+https://github.com/mwalmsley/zoobot.git@for-v1', # use mike's dev/release branch till v1.0 is released
         'requests >= 2.28.1', # used to download prediction images from a remote URL
         'h5py >= 3.7.0' # used for prediction exports
     ]
